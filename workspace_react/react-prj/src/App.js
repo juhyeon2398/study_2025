96cab60138499c85f9d844daec07ae69e6654304
wigeon <juhyeon2398@naver.com>
2025-06-02 14:20:56 +0900

    react 수업1일

---
+++ workspace_react/react-prj/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TestComponent from './components/testComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <TestComponent name='kim'/>
+    </div>
+  );
+}
+
+export default App;
---
+++ workspace_react/react-prj/src/components/testComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const testComponent = (props) => {
+    console.log(" testComponent  :  props", props)
+    return (
+        <div>
+            안녕하세요.
+        </div>
+    );
+};
+
+export default testComponent;
