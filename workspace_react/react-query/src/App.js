1ff9c3ffc27aabce3324402340be994808eb1b3a
wigeon <juhyeon2398@naver.com>
2025-06-19 10:24:38 +0900

    react 수업 11일차

--- workspace_react/react-query/src/App.js
+++ workspace_react/react-query/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import BoardRegister from './components/BoardRegister';
 import Example from './components/Example';
 import InfiniteExample from './components/InfiniteExample';
+import InfiniteScrollExample from './components/InfiniteScrollExample';
 import PostListExample from './components/PostListExample';
 
 function App() {
@@ -10,7 +11,7 @@ function App() {
       {/* <Example number={1}/>
       <Example number={2}/>
       <PostListExample/> */}
-      <InfiniteExample/>
+      <InfiniteScrollExample/>
     </div>
   );
 }
